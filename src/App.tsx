f843413332ee01d01f7f8e1c5137c7466dfff478
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2023-03-15 17:03:00 +0900

    URL path changed

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import Home from "./Routes/Home";
 import Chat from "./Routes/Chat";
 import Image from "./Routes/Image";
 import Header from "./Components/Header";
-import { useEffect } from "react";
+
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/chat" element={<Chat />}></Route>
-        <Route path="/image" element={<Image />}></Route>
+        <Route path="/openAI/" element={<Home />}></Route>
+        <Route path="/openAI/chat" element={<Chat />}></Route>
+        <Route path="/openAI/image" element={<Image />}></Route>
       </Routes>
     </BrowserRouter>
   );
