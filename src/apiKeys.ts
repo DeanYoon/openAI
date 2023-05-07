bc74ec5b8327b34cc4e2fd7a4315dc5ba1083e48
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2023-05-07 17:13:18 +0900

    added dotenv

---
+++ src/apiKeys.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+export const DOMAIN_URL = process.env.DOMAIN_URL;
+
+// export const DOMAIN_URL = "http://localhost:4001";
